3cf66f9178182401684a32cee3b2d4f9eb9c7431
Sofia Dinatolo <92044969+cockatiella@users.noreply.github.com>
2023-07-06 15:32:23 +0200

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,191 @@
+<script setup lang="ts">
+import { ref, onMounted, computed, watch } from 'vue';
+import './HelloWorld.css';
+// Define the types for user objects
+interface User {
+  id: string;
+  name: string;
+  email: string;
+  location: string;
+  gender: string;
+  picture: string;
+}
+
+const username = ref('');
+const searchedUsers = ref<User[]>([]);
+const selectedUser = ref<User | null>(null);
+const loadingMore = ref(false);
+const resultsPerPage = 25;
+const filterText = ref('');
+const filterGender = ref('');
+
+// Save the current page state to localStorage
+function savePageState() {
+  localStorage.setItem('pageState', JSON.stringify({
+    searchedUsers: searchedUsers.value,
+    selectedUser: selectedUser.value,
+    filterText: filterText.value,
+    filterGender: filterGender.value,
+  }));
+}
+
+// Load the saved page state from localStorage
+function loadPageState() {
+  const savedState = localStorage.getItem('pageState');
+  if (savedState) {
+    const { searchedUsers: savedSearchedUsers, selectedUser: savedSelectedUser, filterText: savedFilterText, filterGender: savedFilterGender } = JSON.parse(savedState);
+    searchedUsers.value = savedSearchedUsers;
+    selectedUser.value = savedSelectedUser;
+    filterText.value = savedFilterText;
+    filterGender.value = savedFilterGender;
+  }
+}
+
+// Watch for changes in the page state and save to localStorage
+onMounted(() => {
+  loadPageState();
+});
+
+watch([searchedUsers, selectedUser, filterText, filterGender], () => {
+  savePageState();
+});
+
+async function fetchUsers(results: number) {
+  const response = await fetch(`https://randomuser.me/api/?results=${results}&nat=us&seed=${username.value}`);
+  const data = await response.json();
+  return data.results.map((result: any) => ({
+    id: result.login.uuid,
+    name: `${result.name.first} ${result.name.last}`,
+    email: result.email,
+    phone: result.phone,
+    location: `${result.location.city},
+    ${result.location.state},
+    ${result.location.country}`,
+    gender: result.gender,
+    picture: result.picture.thumbnail,
+  }));
+}
+
+async function searchUser() {
+  searchedUsers.value = await fetchUsers(resultsPerPage);
+}
+
+async function loadMoreResults() {
+  loadingMore.value = true;
+  const newResults = await fetchUsers(resultsPerPage);
+  searchedUsers.value = searchedUsers.value.concat(newResults);
+  loadingMore.value = false;
+}
+
+onMounted(async () => {
+  searchedUsers.value = await fetchUsers(resultsPerPage);
+});
+
+const filteredUsers = computed(() => {
+  let filtered = searchedUsers.value;
+
+  if (filterText.value) {
+    const searchTerm = filterText.value.toLowerCase();
+    filtered = filtered.filter(
+      (user) =>
+        user.name.toLowerCase().includes(searchTerm) ||
+        user.email.toLowerCase().includes(searchTerm)
+    );
+  }
+
+  if (filterGender.value) {
+    filtered = filtered.filter((user) => user.gender === filterGender.value);
+  }
+
+  return filtered;
+});
+
+function formatName(name: string): string {
+  const [firstName, lastName] = name.split(" ");
+  return `${capitalize(firstName)} ${capitalize(lastName)}`;
+}
+
+function capitalize(str: string): string {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+function selectUser(user: User) {
+  selectedUser.value = user;
+}
+
+function closeUserDetails() {
+  selectedUser.value = null; // Clear the selectedUser to hide the user-details-card
+}
+</script>
+
+<template>
+  <div class="wrapper">
+    <div class="container">
+      <h1>Search for a User</h1>
+      <div>
+        <input type="text" v-model="filterText" placeholder="Filter by name" />
+        <select v-model="filterGender">
+          <option value="">All</option>
+          <option value="male">Male</option>
+          <option value="female">Female</option>
+        </select>
+        <button @click="searchUser" class="search">Search</button>
+      </div>
+
+
+      <div class="user-container">
+        <div class="user-list">
+          <ul>
+            <li v-for="(user, index) in filteredUsers" :key="user.id" @click="selectUser(user)">
+              <div class="user-info">
+                <div class="user-thumbnail">
+                  <img :src="user.picture" :alt="user.name" />
+                </div>
+                <div class="user-details">
+                  <div class="user-name">{{ formatName(user.name) }}</div>
+                  <div class="user-email">{{ user.email }}</div>
+                </div>
+              </div>
+              <template v-if="index === filteredUsers.length - 1 && !loadingMore">
+                <div class="more-results-button">
+                <button @click="loadMoreResults">More results...</button>
+                </div>
+              </template>
+           
+            </li>
+          </ul>
+         
+            <div v-if="loadingMore">Loading more results...</div>
+         
+        </div>
+      </div>
+    </div>
+    <div class="user-details-card fixed" v-if="selectedUser">
+      <div class="user-card">
+        <div class="user-thumbnail">
+          <img :src="selectedUser.picture" :alt="selectedUser.name" />
+        </div>
+        <div class="user-card-details">
+          <div class="user-id">
+            <span class="label">ID:</span>
+            <p>{{ selectedUser.id }}</p>
+          </div>
+          <div class="user-location">
+            <span class="label">Location:</span>
+            <p>{{ selectedUser.location }}</p>
+          </div>
+          <div class="user-contact">
+            <span class="label">Contact: </span>
+            <p>{{ selectedUser.email }}</p>
+          </div>
+
+        </div>
+      </div>
+      <button class="close-button search" @click="closeUserDetails">Close</button>
+    </div>
+  </div>
+</template>
+
+<!-- <style scoped>
+
+</style>  -->
